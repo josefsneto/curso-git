0592e6b3e25e4ac3cd3c16f8094d93673d093a1d
Jose Neto <jose@sixit.com.br>
2021-05-19 10:24:56 -0300

    projeto iniciado mostrando ola mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace novo_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
